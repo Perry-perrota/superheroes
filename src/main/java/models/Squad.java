e864b65dda34ce17468ce4e9ced1fd5ed4025ed2
Perry-perrota <perryobara@gmail.com>
2019-08-01 10:14:45 +0300

    adds constructor and getter methods for Squad object

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -1,4 +1,30 @@
 package models;
 
 public class Squad {
+    private int squadId;
+    private int squadSize;
+    private String squadName;
+    private String cause;
+
+    public Squad(int squadSize, String squadName, String cause) {
+        this.squadSize = squadSize;
+        this.squadName = squadName;
+        this.cause = cause;
+    }
+
+    public int getSquadId() {
+        return squadId;
+    }
+
+    public int getSquadSize() {
+        return squadSize;
+    }
+
+    public String getSquadName() {
+        return squadName;
+    }
+
+    public String getCause() {
+        return cause;
+    }
 }
