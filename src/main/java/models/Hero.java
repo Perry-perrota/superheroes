615f61a4f0cd532e90a5a8fc26b58ee24eb5cb12
Perry-perrota <perryobara@gmail.com>
2019-08-01 10:02:05 +0300

    adds constructor and getter methods for Hero object

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,4 +1,36 @@
 package models;
 
 public class Hero {
+    private String heroNAme;
+    private String strength;
+    private String weakness;
+    private int heroID;
+    private int age;
+
+    public Hero(String heroNAme, String strength, String weakness, int age) {
+        this.heroNAme = heroNAme;
+        this.strength = strength;
+        this.weakness = weakness;
+        this.age = age;
+    }
+
+    public String getHeroNAme() {
+        return heroNAme;
+    }
+
+    public String getStrength() {
+        return strength;
+    }
+
+    public String getWeakness() {
+        return weakness;
+    }
+
+    public int getHeroID() {
+        return heroID;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
