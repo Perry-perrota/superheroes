bf2f000dd95ee51878c6e870ac6aefa166b38a9d
Perry-perrota <perryobara@gmail.com>
2019-08-03 18:32:54 +0300

    final commit

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -8,6 +8,7 @@ public class Squad {
     private String squadName;
     private String cause;
     private static ArrayList<Squad> squadInstances=new ArrayList<>();
+    private static ArrayList<Hero> heroes=new ArrayList<>();
 
 
     public Squad(int squadSize, String squadName, String cause) {
@@ -45,5 +46,11 @@ public class Squad {
     }
     public void deleteSquad(){
         squadInstances.remove(squadId-1);
+    }
+    public void addHero(Hero hero){
+       heroes.add(hero);
+    }
+    public static ArrayList<Hero> getSquadHeroes(){
+        return heroes;
     }
 }
